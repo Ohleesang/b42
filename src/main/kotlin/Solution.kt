391450c414cb66114caf988c5ebd6ac2ae325273
Ohleesang <jammmmed@gmail.com>
2024-02-05 09:51:16 +0900

    나머지,나눗셈 연산 이용

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,2 +1,22 @@
+/**
+ * 치킨 10 마리 -> 1개
+ */
+
+
 class Solution {
+    fun solution(chicken: Int): Int {
+        var answer: Int = 0
+        var coupon = chicken
+        while (coupon>=10) {
+            answer += coupon/10
+            coupon = coupon%10 + coupon/10
+        }
+        return answer
+    }
+}
+
+fun main() {
+    val a = Solution()
+    a.solution(100) //11
+    a.solution(1081) // 120
 }
